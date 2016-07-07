31e3df99fd8bd5764d728f190f3705b67c5dafe6
Laurent Falda <l.falda@groupe-scala.com>
2016-07-07 13:21:22 +0200

    first commit

---
+++ src/js/expand.js
@@ -0,0 +1,70 @@
+var selector = function (selectorString, context) {
+  var element = context || document;
+  return element.querySelector(selectorString);
+};
+
+var selectorAll = function (selectorString, context) {
+  var element = context || document;
+  return Array.prototype.slice.call(element.querySelectorAll(selectorString));
+};
+
+var expandFunc = function ($, $$) {
+  'use strict';
+
+  var expand = {
+    init: function (controller) {
+      var inst = Object.create(expand);
+      var mediaQueryList;
+      inst.controller = controller;
+      inst.controlledID = inst.controller.getAttribute('aria-controls');
+      inst.controlled = $('[id="' + inst.controlledID + '"]');
+      inst.controlled.setAttribute('role', 'region');
+      var expandedState = controller.getAttribute('data-expand-default-state');
+      if (expandedState) { inst.controlled.setAttribute('aria-expanded', expandedState); }
+
+      inst.controller.addEventListener('click', function () {
+        inst.setExpanded();
+      });
+
+      var beforePrint = function () {
+        inst.setExpanded(true);
+      };
+
+      if (window.matchMedia) {
+        mediaQueryList = window.matchMedia('print');
+        mediaQueryList.addListener(function (mql) {
+          if (mql.matches) {
+            beforePrint();
+          }
+        });
+      }
+
+      window.onbeforeprint = beforePrint;
+
+      return inst;
+    },
+
+    setExpanded: function (forcedState) {
+      var oldState = this.controlled.getAttribute('aria-expanded') === 'true';
+      var newState = forcedState !== undefined ? forcedState : !oldState;
+      this.controlled.setAttribute('aria-expanded', newState ? 'true' : 'false');
+    }
+  };
+
+  $$('[data-expand]').forEach(function (item) {
+    expand.init(item);
+  });
+};
+
+var stateCheck = setInterval(function () {
+  if (document.readyState === 'complete') {
+    console.log('document is ready');
+    clearInterval(stateCheck);
+
+    // selectorAll('.page-recherche-garants .sidebar div[class*="facetapi"]').forEach(function (item) {
+
+    // });
+
+    expandFunc(selector, selectorAll);
+  }
+}, 100);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const debug = true;
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '.'),
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: {
+    app: [
+      './src/js/expand.js'
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0'],
+          plugins: [
+            'transform-class-properties',
+            'transform-decorators-legacy'
+          ],
+        }
+      }
+    ]
+  },
+  output: {
+    path: `${__dirname}/dev/js`,
+    publicPath: 'js/',
+    filename: 'expand.min.js'
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
